1453837168c4117fde885372e9bf6d4aaf31fbe1
AakashThakur23102000 <aakashthakur20001972@gmail.com>
2024-12-27 02:07:53 +0530

    Changed gradlew version and fixed login ui

--- src/screens/auth/SignIn.tsx
+++ src/screens/auth/SignIn.tsx
@@ -4,7 +4,7 @@ import { ThemeColorsType, ThemeModeType } from '../../store/themeSlice'
 import { useSelector } from 'react-redux'
 import { moderateScale, scale, ScaledSheet, verticalScale } from 'react-native-size-matters'
 import { projectImages, projectPadding } from '../../config/UIConfig'
-import { TextInput } from 'react-native-paper'
+import { Button, TextInput } from 'react-native-paper'
 import CustomText from '../../utils/CustomText'
 import MaterialIcons from "react-native-vector-icons/MaterialIcons"
 
@@ -14,26 +14,30 @@ const SignIn = () => {
 
     const styles = ScaledSheet.create({
         screen: {
-            flexGrow: 1,
+            flex: 1,
             backgroundColor: colors.white,
         },
         logoImage: {
             height: verticalScale(280),
             width: "100%",
             resizeMode: "stretch",
-            borderBottomLeftRadius: scale(25),
-            borderBottomRightRadius: scale(25),
         },
         bottomView: {
             flex: 1,
+            // borderWidth:1,
             // paddingHorizontal: projectPadding.paddingHorizontal,
             // paddingVertical: projectPadding.paddingVertical,
             padding: moderateScale(25),
             rowGap: verticalScale(20)
-
         },
         defaultTextStyle: {
             color: colors.black,
+        },
+        submitButton: {
+            backgroundColor: colors.primary1000,
+            alignSelf: "flex-end",
+            width: "50%",
+            marginTop: "5%"
         }
     })
     return (
@@ -44,32 +48,46 @@ const SignIn = () => {
                 barStyle={selectedThemeMode === "light" ? "dark-content" : "light-content"}
             />
             <ScrollView
-                contentContainerStyle={styles.screen}
+                contentContainerStyle={{ flexGrow: 1 }}
+                showsVerticalScrollIndicator={false}
             >
-                <Image source={projectImages.projectLogo} style={styles.logoImage} />
-                <View style={styles.bottomView}>
-                    <CustomText
-                        customFamily='CormorantUpright'
-                        customSize='extra_extra_large'
-                        customWeight='700'
-                        style={styles.defaultTextStyle}>Login</CustomText>
+                <KeyboardAvoidingView
+                    style={styles.screen}
+                    behavior='position'
+                    keyboardVerticalOffset={0}
+                >
+                    <Image source={projectImages.projectLogo} style={styles.logoImage} />
+                    <View style={styles.bottomView}>
+                        <CustomText
+                            customFamily='CormorantUpright'
+                            customSize='extra_extra_large'
+                            customWeight='700'
+                            style={styles.defaultTextStyle}>Login</CustomText>
 
-                    <TextInput
-                        mode="outlined"
-                        label="User Email"
-                        textContentType='emailAddress'
-                        keyboardType='email-address'
-                        right={<TextInput.Icon icon={() => <MaterialIcons name="remove-red-eye" size={scale(20)} color={colors.placeHolderColor} />} />}
-                    />
-                    <TextInput
-                        mode="outlined"
-                        label="User Password"
-                        textContentType='password'
-                        keyboardType='default'
-                        secureTextEntry
-                        right={<TextInput.Icon  icon={() => <MaterialIcons name="remove-red-eye" size={scale(20)} color={colors.placeHolderColor} />} />}
-                    />
-                </View>
+                        <TextInput
+                            mode="outlined"
+                            label="User Email"
+                            textContentType='emailAddress'
+                            keyboardType='email-address'
+                            right={<TextInput.Icon icon={() => <MaterialIcons name="remove-red-eye" size={scale(20)} color={colors.placeHolderColor} />} />}
+                        />
+                        <TextInput
+                            mode="outlined"
+                            label="User Password"
+                            textContentType='password'
+                            keyboardType='default'
+                            secureTextEntry
+                            right={<TextInput.Icon icon={() => <MaterialIcons name="remove-red-eye" size={scale(20)} color={colors.placeHolderColor} />} />}
+                        />
+                        <Button
+                            onPress={() => { console.log("Hii") }}
+                            loading={false}
+                            mode='elevated'
+                            style={styles.submitButton}
+                            textColor={selectedThemeMode === "light" ? colors.white : colors.black}
+                        >Submit</Button>
+                    </View>
+                </KeyboardAvoidingView>
             </ScrollView>
         </>
     )
